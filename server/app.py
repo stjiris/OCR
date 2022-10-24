33992a98c87480f4bf4a6c89089ee43025563606
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2022-10-24 19:06:00 +0100

    Updates to Server

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS # permitir requests de outros ips alem do servidor
 
+from pdf2image import convert_from_path
 from PIL import Image
 import pytesseract
 
@@ -19,12 +20,19 @@ def submitFile():
 
         # Obter o texto (Tesseract/etc)
         # De momento ainda 
-        text = pytesseract.image_to_string(Image.open("file_uploads/" + file.filename), lang='por')
+        pages = convert_from_path("file_uploads/" + file.filename, 200, poppler_path="C:\\Users\\Andre.LAPTOP01\\Documents\\poppler-22.04.0\\Library\\bin")
+        pages[0].save("file_uploads/page1.jpg", "JPEG")
+        pages[1].save("file_uploads/page2.jpg", "JPEG")
+        # text = pytesseract.image_to_string(Image.open("file_uploads/" + file.filename), lang='por')
+        text = pytesseract.image_to_string(pages[0], lang='por')
+        # text = "Hello World"
 
         with open(filename_txt, "w") as f:
             f.write(text)
 
         # Enviar texto para o servidor
+
+        print("Acabei")
         return {"success": True, "text": text}
     except Exception as e:
         print(e)
