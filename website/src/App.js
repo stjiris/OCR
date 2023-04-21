0ec3a1f0fc84d4b78b65472cb855dd88f666ad0f
Luís António <luis.antonio.lima.silva@gmail.com>
2023-04-21 20:18:41 +0100

    Update version

--- website/src/App.js
+++ website/src/App.js
@@ -19,7 +19,7 @@ import InfoIcon from '@mui/icons-material/Info';
  * PATCH version when you make backwards compatible bug fixes
  */
 
-const VERSION = "0.9.3";
+const VERSION = "0.9.4";
 
 function App() {
   class Form extends React.Component {
