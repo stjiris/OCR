c922080d898f5bbaf4cdf0f3e03dcc32349f95fc
Luís António <luis.antonio.lima.silva@gmail.com>
2023-05-09 20:53:17 +0100

    Revamp private session modal

--- website/src/Components/Form/PrivateSessionMenu.js
+++ website/src/Components/Form/PrivateSessionMenu.js
@@ -13,7 +13,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 600,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -40,12 +40,20 @@ class PrivateSessionMenu extends React.Component {
         this.errorNot = React.createRef();
     }
 
+    checkHasFile() {
+        if (this.state.filesystem.rowRefs.length !== 0){
+            return true;
+        }
+        return false;
+    }
+
     toggleOpen() {
         this.setState({ open: !this.state.open });
     }
 
     createPrivateSession() {
-        this.setState({ buttonDisabled: true, open: false });        
+        this.setState({ buttonDisabled: true, open: false });
+        this.state.filesystem.createFile();        
     }
 
     render() {
@@ -56,23 +64,42 @@ class PrivateSessionMenu extends React.Component {
                 <Modal open={this.state.open}>
                     <Box sx={style}>
                         <Typography id="modal-modal-title" variant="h6" component="h2">
-                            Sessão privada
+                            <b>Sessão privada</b>
                         </Typography>
                         <Typography variant="body1" component="p" sx={{mt: '1rem', mb: '1rem'}}>
-                            Para poder aceder a esta sessão novamente no futuro, certifique-se de guardar o link da sessão privada (<b>{window.location.href}</b>) num lugar seguro. Caso perca o link, não conseguirá aceder novamente a esta sessão privada.
+                            Para poder aceder a esta sessão novamente no futuro, certifique-se que guarda o link da sessão privada num lugar seguro. 
                         </Typography>
-                        <Button
-                            disabled={this.state.buttonDisabled}
-                            variant="contained"
-                            sx={{border: '1px solid black', mt: '0.5rem', mr: '1rem'}}
-                            onClick={() => this.createPrivateSession()}
-                        >
-                            OK
-                        </Button>
-
-                        <IconButton disabled={this.state.buttonDisabled} sx={crossStyle} aria-label="close" onClick={() => this.toggleOpen()}>
-                            <CloseRoundedIcon />
-                        </IconButton>
+                        <Typography variant="body1" component="p" sx={{mt: '1rem', mb: '1rem'}}>
+                            <b>{window.location.href}</b>
+                        </Typography>
+                        <Typography variant="body1" component="p" sx={{mt: '1rem', mb: '1rem'}}>
+                            Caso perca o link, não conseguirá aceder novamente a esta sessão privada.
+                        </Typography>
+                        <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <Button
+                                disabled={this.state.buttonDisabled}
+                                variant="contained"
+                                sx={{border: '1px solid black', mt: '0.5rem'}}
+                                onClick={() => navigator.clipboard.writeText(window.location.href)}
+                            >
+                                Copiar link para a área de transferência
+                            </Button>
+                            {!this.checkHasFile() &&
+                                <Button
+                                    disabled={this.checkHasFile()}
+                                    variant="contained"
+                                    sx={{border: '1px solid black', ml: '0.5rem', mt: '0.5rem'}}
+                                    onClick={() => this.createPrivateSession()}
+                                >
+                                    Adicionar documento
+                                </Button>
+                            }
+                            {this.checkHasFile() &&
+                                <IconButton disabled={this.state.buttonDisabled} sx={crossStyle} aria-label="close" onClick={() => this.toggleOpen()}>
+                                    <CloseRoundedIcon />
+                                </IconButton>
+                            }                          
+                        </Box>
                     </Box>
                 </Modal>
             </Box>
