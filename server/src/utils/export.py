77bc6cb0e9fd2557b70be64162c40da8ccef64b1
Gui Branco <72111080+AdventurousGui@users.noreply.github.com>
2025-05-15 00:28:47 +0100

    fix reference to path of original file

--- server/src/utils/export.py
+++ server/src/utils/export.py
@@ -190,7 +190,7 @@ def export_pdf(path, force_recreate=False, simple=False, get_csv=False):
             page_extension = "jpg"
             pdf_basename = get_file_basename(path)
 
-            pdf = pdfium.PdfDocument(f"{path}/_export/{pdf_basename}.pdf")
+            pdf = pdfium.PdfDocument(f"{path}/{pdf_basename}.pdf")
             for i in range(len(pdf)):
                 page = pdf[i]
                 bitmap = page.render(dpi_compressed / 72)
